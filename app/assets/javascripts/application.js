a301586133bcffcc2c3e1ac040f2b280497abc53
Scott Ford <uofoduck14@gmail.com>
2019-05-13 18:24:38 +0000

    navbar add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require popper_js
+//= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
