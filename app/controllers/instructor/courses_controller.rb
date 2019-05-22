7c2def3e71b865cbcf03cc78b7f6d3df446ab4d5
Scott Ford <uofoduck14@gmail.com>
2019-05-22 22:17:14 +0000

    show page

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,10 +6,19 @@ class Instructor::CoursesController < ApplicationController
     @course = Course.new
   end
 
-  def show
+  def create
+    @course = current_user.courses.create(course_params)
+    redirect_to instructor_course_path(@course)
   end
 
-  def create
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
   end
 
 end
