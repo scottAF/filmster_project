d0d05b7dc3df0ec8d14c6503576a57ce454e186e
Scott Ford <uofoduck14@gmail.com>
2019-06-03 18:47:21 +0000

    images model and fixed DB issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_223118) do
+ActiveRecord::Schema.define(version: 2019_06_03_183218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,16 @@ ActiveRecord::Schema.define(version: 2019_05_28_223118) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "lessons", force: :cascade do |t|
