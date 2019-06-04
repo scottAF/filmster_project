7e85c28d3f279984bbe2425bf941df5e0571eb49
Scott Ford <uofoduck14@gmail.com>
2019-06-04 00:23:51 +0000

    deleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
-  resources :images, only: :show
-  namespace :instructor do
+    namespace :instructor do
     resources :sections, only: [] do 
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
-      resources :images, only:[:create, :show]
     end
   end
 end
