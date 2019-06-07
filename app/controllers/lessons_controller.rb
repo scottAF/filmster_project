84d8ab0ce2991c210b3f8bf768bda673fc876a8c
Scott Ford <uofoduck14@gmail.com>
2019-06-07 20:45:15 +0000

    Must be enrolled to view lesson added for users

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course, only: [:show]
 
   def show
   end
@@ -9,4 +11,17 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  helper_method :current_course
+  def current_course
+    current_lesson.section.course
+  end
+
+
+  def require_enrolled_for_current_course
+    if ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must Enroll in course for lessons'
+    end
+  end
+
 end
